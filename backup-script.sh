3d69a9a7893920edb2716c8166645f17904dbb46
varunbheemaiah <32514558+varunbheemaiah@users.noreply.github.com>
2020-12-27 10:22:34 +0530

    Update backup-script.sh

--- backup-script.sh
+++ backup-script.sh
@@ -5,6 +5,6 @@ git config --global user.email "dummy-email"
 cd /data/world
 git add .
 
-DATE_NOW="`date +%d-%b-%Y`"
+DATE_NOW="`date +%T %d-%b-%Y`"
 git commit -m"automated backup for $DATE_NOW"
 git push origin master
